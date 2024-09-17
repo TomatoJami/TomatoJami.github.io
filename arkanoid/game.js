097cc70744ae4ede51eb949b369c61150a58b15c
MaksimS <m.shchepelev1@gmail.com>
2024-09-17 09:32:52 +0300

    third

--- arkanoid/game.js
+++ arkanoid/game.js
@@ -1,5 +1,7 @@
 let game = {
     ctx: null,
+    platform: null,
+    ball: null,
     sprites: {
         background: null,
         ball: null,
@@ -24,16 +26,15 @@ let game = {
         }
     },
     run() {
-        const loop = () => {
+        window.requestAnimationFrame(() => {
             this.render();
-            window.requestAnimationFrame(loop);
-        };
-        loop();
-    },
+        });
+    },    
     render() {
         this.ctx.drawImage(this.sprites.background, 0, 0);
-        this.ctx.drawImage(this.sprites.ball, 0, 0);
-        this.ctx.drawImage(this.sprites.platform, 0, 0);
+        this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
+            this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+        this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
     },
     start: function() {
         this.init();
@@ -43,4 +44,18 @@ let game = {
     }
 };
 
-game.start();
+game.ball = {
+    x: 320,
+    y: 280,
+    width: 20,
+    height: 20
+};
+
+game.platform = {
+    x: 280,
+    y: 300
+};
+
+window.addEventListener('load', () => {
+    game.start();
+})
