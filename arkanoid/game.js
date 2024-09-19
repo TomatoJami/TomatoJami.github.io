24dfb5db83fda7b26083ea669532948f24ac2f9d
MaksimS <m.shchepelev1@gmail.com>
2024-09-19 13:01:22 +0300

    2 part 7

--- arkanoid/game.js
+++ arkanoid/game.js
@@ -26,7 +26,12 @@ let game = {
     },
     init: function() {
         this.ctx = document.getElementById('mycanvas').getContext('2d');
+        this.setTextFont();
         this.setEvents();
+    },
+    setTextFont() {
+        this.ctx.font = '20px Arial';
+        this.ctx.fillStyle = '#FFFFFF';
     },
     setEvents() {
         window.addEventListener('keydown', e => {
@@ -127,6 +132,7 @@ let game = {
             this.ball.x, this.ball.y, this.ball.width, this.ball.height);
         this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
         this.renderBlocks();
+        this.ctx.fillText('Score:' + this.score, 15, 20);
     },
     renderBlocks() {
         for (let block of this.blocks) {
