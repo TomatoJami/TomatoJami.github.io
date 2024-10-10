73276d5c01938530890cfe7a0cc142412dad261d
MaksimS <m.shchepelev1@gmail.com>
2024-10-10 15:07:35 +0300

    reactttt

---
+++ react_1/build/precache-manifest.1debe1f09d41969e4ec6fa8394ef55a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "97a62fb4a72e30bd16c2",
+    "url": "/react1/static/css/main.2c21554c.chunk.css"
+  },
+  {
+    "revision": "97a62fb4a72e30bd16c2",
+    "url": "/react1/static/js/main.97a62fb4.chunk.js"
+  },
+  {
+    "revision": "a83ff9f0ce1af363ac96",
+    "url": "/react1/static/css/1.daf8512b.chunk.css"
+  },
+  {
+    "revision": "a83ff9f0ce1af363ac96",
+    "url": "/react1/static/js/1.a83ff9f0.chunk.js"
+  },
+  {
+    "revision": "a74937258285e9ed0a2e",
+    "url": "/react1/static/js/runtime~main.a7493725.js"
+  },
+  {
+    "revision": "01ba3696cd89ec29a43a82e47d73ad46",
+    "url": "/react1/index.html"
+  }
+];
