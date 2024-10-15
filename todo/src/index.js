0721a1738db6252136d564dd6d463bf69a9ef563
MaksimS <m.shchepelev1@gmail.com>
2024-10-15 09:50:46 +0300

    react hahha

--- todo/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-import App from './components/app/';
-
-// Рендерим компонент App в элемент с id 'root'
-ReactDOM.render(<App />, document.getElementById('root'));
