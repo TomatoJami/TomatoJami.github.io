36523fc709fa4a38d4b33afc592f0211f0fadd63
MaksimS <m.shchepelev1@gmail.com>
2024-10-16 15:02:57 +0300

    ice and fire

---
+++ IceAndFire/precache-manifest.5334410ecf718b872284bef6637bd854.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "75ddea72986d6dbcaafb",
+    "url": "/IceAndFire/static/css/main.aedb0cb1.chunk.css"
+  },
+  {
+    "revision": "75ddea72986d6dbcaafb",
+    "url": "/IceAndFire/static/js/main.75ddea72.chunk.js"
+  },
+  {
+    "revision": "52836d29622385926599",
+    "url": "/IceAndFire/static/css/1.b21e5b27.chunk.css"
+  },
+  {
+    "revision": "52836d29622385926599",
+    "url": "/IceAndFire/static/js/1.52836d29.chunk.js"
+  },
+  {
+    "revision": "7acf65fb3ce37e1d44a7",
+    "url": "/IceAndFire/static/js/runtime~main.7acf65fb.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/IceAndFire/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/IceAndFire/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "215820c72c5f627193f9af60c9051811",
+    "url": "/IceAndFire/index.html"
+  }
+];
