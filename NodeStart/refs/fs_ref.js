e2dc47f73666673064ac6ae63548289b53c33655
MaksimS <m.shchepelev1@gmail.com>
2024-11-11 15:06:47 +0200

    nodestart 3

--- NodeStart/refs/fs_ref.js
+++ NodeStart/refs/fs_ref.js
@@ -1,29 +1,75 @@
 const fs = require('fs')
 const path = require('path')
 
+// // Создаём папку 'notes'. Если ошибка при создании папки, она будет выброшена.
+// fs.mkdir(path.join(__dirname, 'notes'), err => {
+//     if (err) throw err
 
-fs.mkdir(path.join(__dirname, 'notes'), err => {
+//     console.log('Папка была создана')
+// })
+
+// // Создаём файл 'mynotes.txt' в папке 'notes' с текстом 'Hello world'.
+// fs.writeFile(
+//     path.join(__dirname, 'notes', 'mynotes.txt'),
+//     'Hello world',
+//     (err) => {
+//         if (err) throw err
+//         console.log('Файл был создан')
+
+//         // Добавляем в файл строку ' From append file'.
+//         fs.appendFile(
+//             path.join(__dirname, 'notes', 'mynotes.txt'),
+//             ' From append file',
+//             err => {
+//                 if (err) throw err
+//                 console.log("Файл был изменён")
+
+//                 // Читаем содержимое файла и выводим в консоль.
+//                 fs.readFile(
+//                     path.join(__dirname, 'notes', 'mynotes.txt'),
+//                     'utf-8',
+//                     (err, data) => {
+//                         if (err) throw err
+//                         console.log(data)
+//                     }
+//                 )
+//             }
+//        )
+//     }
+// )
+
+// fs.rename(
+//     path.join(__dirname, 'notes', 'mynotes.txt'),
+//     path.join(__dirname, 'notes', 'notes.txt'),
+//     err => {
+//         if (err) throw err
+
+//         console.log('Файл переименован')
+//     }
+// )
+
+fs.mkdir(path.join(__dirname, 'test'), err => {
     if (err) throw err
 
     console.log('Папка была создана')
 })
 
 fs.writeFile(
-    path.join(__dirname, 'notes', 'mynotes.txt'),
-    'Hello world',
+    path.join(__dirname, 'test', 'test.txt'),
+    'SPTV',
     (err) => {
         if (err) throw err
         console.log('Файл был создан')
 
         fs.appendFile(
-            path.join(__dirname, 'notes', 'mynotes.txt'),
-            ' From append file',
+            path.join(__dirname, 'test', 'test.txt'),
+            '24',
             err => {
                 if (err) throw err
                 console.log("Файл был изменён")
 
                 fs.readFile(
-                    path.join(__dirname, 'notes', 'mynotes.txt'),
+                    path.join(__dirname, 'test', 'test.txt'),
                     'utf-8',
                     (err, data) => {
                         if (err) throw err
--- NodeStart/refs/path_ref.js
+++ NodeStart/refs/path_ref.js
@@ -1,12 +1,30 @@
 const path = require('path');
 
-console.log(__filename)
-console.log(path.basename(__filename)) 
-console.log(path.dirname(__filename))
-console.log(path.extname(__filename))
-
-console.log(path.parse(__filename))
+console.log(__filename);
+// Выводит: абсолютный путь к текущему файлу
 
 
-console.log(path.join(__dirname, 'test', 'second.html'))
-console.log(path.resolve(__dirname, './test', '/second.html'))
+console.log(path.basename(__filename)); 
+// Выводит: имя файла, т.е. 'index.js'
+
+console.log(path.dirname(__filename));
+// Выводит: путь к директории, в которой расположен текущий файл
+
+console.log(path.extname(__filename));
+// Выводит: расширение файла, '.js'
+
+console.log(path.parse(__filename));
+// Выводит объект, содержащий:
+// {
+//   root: '/',
+//   dir: '/home/user/project',
+//   base: 'index.js',
+//   ext: '.js',
+//   name: 'index'
+// }
+
+console.log(path.join(__dirname, 'test', 'second.html'));
+// Выводит путь, объединяя текущую директорию (__dirname) с 'test' и 'second.html'.
+
+console.log(path.resolve(__dirname, './test', '/second.html'));
+// Выводит абсолютный путь, разрешая все компоненты
